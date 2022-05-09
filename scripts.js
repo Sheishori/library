f66173294970019e7b9b82fdd999335571b5232f
Sheishori <sheishori@gmail.com>
2022-05-09 13:47:58 +0200

    Add function displaying books on the page

--- scripts.js
+++ scripts.js
@@ -18,9 +18,26 @@ function addBookToLibrary() {
 	let read = prompt("Read status:");
 	let newBook = new Book(title, author, pages, read);
 	myLibrary.push(newBook);
+	updateLibrary();
 }
 
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'No');
 myLibrary.push(theHobbit);
 
-console.log(theHobbit.info());
+const bookshelf = document.querySelector("#bookshelf");
+
+function updateLibrary() {
+	for (let book in myLibrary) {
+		let bookCard = document.createElement('div');
+		for (let property in myLibrary[book]) {
+			if (myLibrary[book].hasOwnProperty(property)) {
+				let bookProperty = document.createElement('div');
+				bookProperty.textContent = `${property.charAt(0).toUpperCase() + property.slice(1)}: ${myLibrary[book][property]}`;
+				bookCard.append(bookProperty);
+			}
+		}
+		bookshelf.append(bookCard);
+	}
+}
+
+updateLibrary();

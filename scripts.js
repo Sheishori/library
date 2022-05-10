0577f82b41d1ac51f28fa9051f1080cbba8287e8
Sheishori <sheishori@gmail.com>
2022-05-10 11:43:38 +0200

    Add a 'remove' button to book cards

--- scripts.js
+++ scripts.js
@@ -23,6 +23,9 @@ function addBookToLibrary() {
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'No');
 myLibrary.push(theHobbit);
+myLibrary.push(theHobbit);
+myLibrary.push(theHobbit);
+myLibrary.push(theHobbit);
 
 const bookshelf = document.querySelector("#bookshelf");
 const addBook = document.querySelector('.add-book');
@@ -33,6 +36,7 @@ function updateLibrary() {
 	for (let book in myLibrary) {
 		let bookCard = document.createElement('div');
 		bookCard.classList.add('card');
+		bookCard.classList.add(`id-${book}`);
 		for (let property in myLibrary[book]) {
 			if (myLibrary[book].hasOwnProperty(property)) {
 				let bookProperty = document.createElement('div');
@@ -40,6 +44,10 @@ function updateLibrary() {
 				bookCard.append(bookProperty);
 			}
 		}
+		let deleteButton = document.createElement('button');
+		deleteButton.classList.add("remove");
+		deleteButton.textContent = "Remove";
+		bookCard.append(deleteButton);
 		bookshelf.append(bookCard);
 	}
 }

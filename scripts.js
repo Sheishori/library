c9ab692bb85df7daa287e47f6200eb1913a5a4ee
Sheishori <sheishori@gmail.com>
2022-05-10 14:27:39 +0200

    Add buttons enabling changing of the read status on the books

--- scripts.js
+++ scripts.js
@@ -11,6 +11,10 @@ Book.prototype.info = function() {
 		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
 
+Book.prototype.changeStatus = function() {
+	this.read = (this.read === 'Yes') ? 'No' : 'Yes';
+}
+
 const bookForm = document.querySelector('#book-form');
 const formElement = document.querySelector('form');
 formElement.addEventListener("submit", (event) => {
@@ -31,10 +35,13 @@ function addBookToLibrary() {
 }
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'No');
+let lotr1 = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', '479', 'No');
+let lotr2 = new Book('The Two Towers', 'J.R.R. Tolkien', '415', 'No');
+let lotr3 = new Book('The Return of the King', 'J.R.R. Tolkien', '347', 'No');
 myLibrary.push(theHobbit);
-myLibrary.push(theHobbit);
-myLibrary.push(theHobbit);
-myLibrary.push(theHobbit);
+myLibrary.push(lotr1);
+myLibrary.push(lotr2);
+myLibrary.push(lotr3);
 
 const bookshelf = document.querySelector("#bookshelf");
 const addBook = document.querySelector('.add-book');
@@ -56,6 +63,15 @@ function updateLibrary() {
 		let deleteButton = document.createElement('button');
 		deleteButton.classList.add("remove");
 		deleteButton.textContent = "Remove";
+		let readButton = document.createElement('button');
+		readButton.classList.add("read-status");
+		readButton.textContent = "Read status";
+		readButton.addEventListener("click", (e) => {
+			let cardId = e.target.parentElement.classList.value.match(/id-[0-9]+/g)[0].split("-")[1];
+			myLibrary[cardId].changeStatus();
+			updateLibrary();
+		})
+		bookCard.append(readButton);
 		bookCard.append(deleteButton);
 		bookshelf.append(bookCard);
 	}
